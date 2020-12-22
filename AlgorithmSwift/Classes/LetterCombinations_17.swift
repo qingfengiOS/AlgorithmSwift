e2dcc14dd557b8f0509a83ba6d1a88a7c0b690cb
Aiden <aiden.li@kucoin.com>
2020-12-22 16:25:38 +0800

    fix error

--- AlgorithmSwift/Classes/LetterCombinations_17.swift
+++ AlgorithmSwift/Classes/LetterCombinations_17.swift
@@ -26,7 +26,7 @@ class LetterCombinations_17: BaseViewController {
         "1": [""],
         "2": ["a", "b", "c"],
         "3": ["d", "e", "f"],
-        "4": ["g", "h", ""],
+        "4": ["g", "h", "i"],
         "5": ["j", "k", "l"],
         "6": ["m", "n", "o"],
         "7": ["p", "q", "r", "s"],
